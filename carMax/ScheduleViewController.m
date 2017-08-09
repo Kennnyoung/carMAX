bdbc6ec5386f2dcf37a38d63fb6253bce4d6b75b
Yuhao Kan <kyh649782182@gmail.com>
2017-08-09 16:52:35 -0400

    set up schedule view controller

--- carMax/ScheduleViewController.m
+++ carMax/ScheduleViewController.m
@@ -13,7 +13,38 @@
 @interface ScheduleViewController ()
 
 @property (weak, nonatomic) IBOutlet UILabel *month;
-
+@property (weak, nonatomic) IBOutlet UIButton *first;
+@property (weak, nonatomic) IBOutlet UIButton *second;
+@property (weak, nonatomic) IBOutlet UIButton *third;
+@property (weak, nonatomic) IBOutlet UIButton *fourth;
+@property (weak, nonatomic) IBOutlet UIButton *fifth;
+@property (weak, nonatomic) IBOutlet UIButton *sixth;
+@property (weak, nonatomic) IBOutlet UIButton *seventh;
+@property (weak, nonatomic) IBOutlet UIButton *eighth;
+@property (weak, nonatomic) IBOutlet UIButton *ninth;
+@property (weak, nonatomic) IBOutlet UIButton *tenth;
+@property (weak, nonatomic) IBOutlet UIButton *eleventh;
+@property (weak, nonatomic) IBOutlet UIButton *twelfth;
+@property (weak, nonatomic) IBOutlet UIButton *thirteenth;
+@property (weak, nonatomic) IBOutlet UIButton *fourteenth;
+@property (weak, nonatomic) IBOutlet UIButton *fifteenth;
+@property (weak, nonatomic) IBOutlet UIButton *sixteenth;
+@property (weak, nonatomic) IBOutlet UIButton *seventeenth;
+@property (weak, nonatomic) IBOutlet UIButton *eighteenth;
+@property (weak, nonatomic) IBOutlet UIButton *ninteenth;
+@property (weak, nonatomic) IBOutlet UIButton *twentyth;
+@property (weak, nonatomic) IBOutlet UIButton *twentyFirst;
+@property (weak, nonatomic) IBOutlet UIButton *twentySecond;
+@property (weak, nonatomic) IBOutlet UIButton *twentyThird;
+@property (weak, nonatomic) IBOutlet UIButton *twentyFourth;
+@property (weak, nonatomic) IBOutlet UIButton *twentyFifth;
+@property (weak, nonatomic) IBOutlet UIButton *twenthSixth;
+@property (weak, nonatomic) IBOutlet UIButton *twentySeventh;
+@property (weak, nonatomic) IBOutlet UIButton *twentyEighth;
+@property (weak, nonatomic) IBOutlet UIButton *twentyNinth;
+@property (weak, nonatomic) IBOutlet UIButton *thirtyth;
+@property (weak, nonatomic) IBOutlet UIButton *thirtyFirst;
+@property NSArray *dateArray;
 @end
 
 @implementation ScheduleViewController
@@ -28,7 +59,7 @@
     NSInteger day = [components day];
     NSInteger month = [components month];
     NSInteger year = [components year];
-    
+    self.dateArray = [[NSArray alloc] initWithObjects:_first,_second,_third,_fourth,_fifth,_sixth,_seventh,_eighth,_ninth,_tenth,_eleventh,_twelfth,_thirteenth,_fourteenth,_fifteenth,_sixteenth,_seventeenth,_eighteenth,_ninteenth,_twentyth,_twentyFirst,_twentySecond,_twentyThird,_twentyFourth,_twentyFifth,_twenthSixth,_twentySeventh,_twentyEighth,_twentyNinth,_thirtyth,_thirtyFirst,nil];
     switch (month) {
         case 1:
             self.month.text = @"January";
@@ -67,6 +98,24 @@
             self.month.text = @"December";
         default:
             break;
+    }
+    
+    for (int i =0; i< day-1;i++){
+        UIButton *disable = [self.dateArray objectAtIndex:i];
+        disable.enabled = NO;
+    }
+    
+    if (month == 2 || month == 4 || month == 6 || month == 9 || month == 11){
+        if (month == 2){
+            if (year % 4 != 0){
+                UIButton *twentyNine = [self.dateArray objectAtIndex:28];
+                twentyNine.hidden = YES;
+            }
+            UIButton *thirty = [self.dateArray objectAtIndex:29];
+            thirty.hidden = YES;
+        }
+        UIButton *thirtyOne = [self.dateArray objectAtIndex:30];
+        thirtyOne.hidden = YES;
     }
     
     NSLog(@"day: %lu, month: %lu, year: %lu\n", day, month, year);
